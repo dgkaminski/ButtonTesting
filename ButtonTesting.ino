9259db4f9d83e1a3055164fe5bb77e14d8e6bece
dgkaminski <dgkaminski@wpi.edu>
2022-04-11 01:57:47 -0400

    got everything working

--- ButtonTesting.ino
+++ ButtonTesting.ino
@@ -1,4 +1,4 @@
-xxz#include <Servo.h>
+#include <Servo.h>
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
@@ -9,21 +9,31 @@ Servo spinWheel;
 #define ButtonForward 8
 #define ButtonBackward 7
 #define LED 6
+#define SERVOMIN  75 // This is the 'minimum' pulse length count (out of 4096)
+#define SERVOMAX  435 // This is the 'maximum' pulse length count (out of 4096) //BOTH ABOVE FOR ARM
+#define SERVO_FREQ 50 // Analog servos run at ~50 Hz updates
+#define USMIN  2000 // This is the rounded 'minimum' microsecond length based on the minimum pulse of 150
+#define USMAX  2400 // This is the rounded 'maximum' microsecond length based on the maximum pulse of 600
 int lastButtonStateForward;    // the previous state of button
 int currentButtonStateForward; // the current state of button
 int lastButtonStateBackward;    // the previous state of button
 int currentButtonStateBackward; // the current state of button
+int lastDirection; //The last direction that the arm was in, if it's at 1 was at right of book
 
 void setup()   {
   pinMode(6, OUTPUT);
   pinMode(8, INPUT);
+  pwm.begin();
   Serial.begin(9600);
-  pmw.begin();
-  spinWheel.attach(0,480,2400);
-  spinArm.attach(1,480,2400);
+  //spinArm.attach(0, 480, 2400);
+  //spinWheel.attach(1, 480, 2400);
+  pwm.setOscillatorFrequency(27000000);
+  pwm.setPWMFreq(SERVO_FREQ);  // Analog servos run at ~50 Hz updates
+  lastDirection = 0; //Set by default to be on right side of book
 }
 
 void loop() {
+
   lastButtonStateForward    = currentButtonStateForward;      // save the last state
   lastButtonStateBackward    = currentButtonStateBackward;      // save the last state
   currentButtonStateForward = digitalRead(ButtonForward); // read new state
@@ -31,18 +41,37 @@ void loop() {
   if (lastButtonStateForward == HIGH && currentButtonStateForward == LOW) { //Seems to do this after the button has been released
     digitalWrite(LED, HIGH);
     Serial.println("The forward button is pressed");
-    for (angleArm = 0; angleArm <= 180; angleArm += 1) { //This loop moves the servo 180 degrees forwards/clockwise
-      spinArm.write(angleArm);
-      delay(5);
+    if (lastDirection == 0) {
+      for (int pulselen = SERVOMIN; pulselen < SERVOMAX; pulselen++) {
+        pwm.setPWM(1, 0, pulselen); //1 SHOULD BE THE PIN FOR THE ARM
+      }
+      lastDirection = 1;
     }
-    for (angleWheel = 0; angleWheel <= 180; angleWheel += 1) { //This loop moves the servo 180 degrees forwards/clockwise
-      spinWheel.write(angleWheel);
-      delay(5);
+    delay(1000);
+    for (int time = 2 ; time > 0; time--) {
+      for (uint16_t microsec = USMAX; microsec > USMIN; microsec--) {
+        pwm.writeMicroseconds(0, microsec);
+      }
+      pwm.writeMicroseconds(0, 0);
     }
   }
   else if (lastButtonStateBackward == HIGH && currentButtonStateBackward == LOW) {
     digitalWrite(LED, HIGH);
     Serial.println("The backward button is pressed");
+    if (lastDirection == 1) {
+      for (int pulselen = SERVOMAX; pulselen > SERVOMIN; pulselen--) {
+        pwm.setPWM(1, 0, pulselen); //1 SHOULD BE THE PIN FOR THE ARM
+      }
+      lastDirection = 0;
+    }
+    delay(1000);
+    for (int time = 2 ; time > 0; time--) {
+      for (uint16_t microsec = 1000; microsec > 500; microsec--) {
+        pwm.writeMicroseconds(0, microsec);
+      }
+      delay(1);
+      pwm.writeMicroseconds(0, 0);
+    }
   }
   else {
     digitalWrite(6, LOW);
